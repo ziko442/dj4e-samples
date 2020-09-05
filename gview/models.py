9559ed67ddbdee18ec0351fe4f9323d046c97ebd
ziko442 <ziko442@gmail.com>
2020-09-06 00:00:52 +0100

    my first commit

--- gview/models.py
+++ gview/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-from django.db import models
+#from django.db import models
 
 class Cat(models.Model):
     name = models.CharField(max_length=128)
